50d4e59a97100c0c807ac2acaf809a3172f9b06f
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-10-30 10:42:34 +0300

    create HomeScreen

---
+++ composeApp/src/commonMain/kotlin/org/example/currency_tracker/ui/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package org.example.currency_tracker.ui.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import cafe.adriel.voyager.core.model.rememberScreenModel
+import cafe.adriel.voyager.core.screen.Screen
+import org.example.currency_tracker.ui.composable.ContentError
+import org.example.currency_tracker.ui.composable.ContentLoading
+import org.example.currency_tracker.ui.composable.ContentVisible
+import org.example.currency_tracker.ui.home.view_model.HomeInteractions
+import org.example.currency_tracker.ui.home.view_model.HomeUiState
+import org.example.currency_tracker.ui.home.view_model.HomeViewModel
+import org.example.currency_tracker.ui.shared_ui.ContentStatus
+import org.example.currency_tracker.ui.util.EventHandler
+
+
+class HomeScreen : Screen {
+    @Composable
+    override fun Content() {
+        val viewModel = rememberScreenModel { HomeViewModel() }
+        val state by viewModel.state.collectAsStateWithLifecycle()
+        EventHandler(viewModel.event) { event, _ ->
+
+        }
+        HomeContent(state = state, interactions = viewModel)
+    }
+}
+
+@Composable
+private fun HomeContent(
+    state: HomeUiState,
+    interactions: HomeInteractions,
+) {
+    ContentLoading(isVisible = state.contentStatus == ContentStatus.LOADING)
+    ContentVisible(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+        }
+    }
+    ContentError(
+        isVisible = state.contentStatus == ContentStatus.FAILURE,
+        onTryAgain = {}
+    )
+}
--- composeApp/src/commonMain/kotlin/org/example/currency_tracker/ui/home/view_model/HomeUiState.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_tracker/ui/home/view_model/HomeUiState.kt
@@ -1,7 +1,9 @@
 package org.example.currency_tracker.ui.home.view_model
 
+import androidx.compose.runtime.Immutable
 import org.example.currency_tracker.ui.shared_ui.ContentStatus
 
+@Immutable
 data class HomeUiState(
     val contentStatus: ContentStatus = ContentStatus.LOADING,
 ) 
