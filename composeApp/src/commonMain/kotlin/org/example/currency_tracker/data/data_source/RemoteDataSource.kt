da7790d13f0a64e31180e476b2e97a5c2315f69c
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-10-30 21:25:11 +0300

    add getCoinList to repo

--- composeApp/src/commonMain/kotlin/org/example/currency_tracker/data/data_source/RemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package org.example.currency_tracker.data.data_source
-
-import org.example.currency_tracker.data.model.CoinDto
-
-interface RemoteDataSource {
-
-}
--- composeApp/src/commonMain/kotlin/org/example/currency_tracker/data/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_tracker/data/repository/Repository.kt
@@ -1,4 +1,7 @@
 package org.example.currency_tracker.data.repository
 
+import org.example.currency_tracker.data.model.CoinDto
+
 interface Repository {
+    suspend fun getCoinList(): List<CoinDto>
 }
