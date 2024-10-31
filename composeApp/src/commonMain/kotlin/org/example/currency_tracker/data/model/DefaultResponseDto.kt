3d04914cab0c217f570915e718bcc6c35c72dfd1
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-10-31 19:27:08 +0300

    update coinResponse to be default response

--- composeApp/src/commonMain/kotlin/org/example/currency_tracker/data/model/DefaultResponseDto.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_tracker/data/model/DefaultResponseDto.kt
@@ -3,6 +3,6 @@ package org.example.currency_tracker.data.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CoinResponseDto(
-    val data: List<CoinDto>,
+data class DefaultResponseDto<out T>(
+    val data: List<T>,
 )
