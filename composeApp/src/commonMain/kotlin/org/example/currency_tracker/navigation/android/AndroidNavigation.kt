a803c7b6b593e8408af853a2e0987dba96c0d02a
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-10-31 20:31:30 +0300

    separate android design

--- composeApp/src/commonMain/kotlin/org/example/currency_tracker/navigation/android/AndroidNavigation.kt
+++ composeApp/src/commonMain/kotlin/org/example/currency_tracker/navigation/android/AndroidNavigation.kt
@@ -1,5 +1,6 @@
 package org.example.currency_tracker.navigation.android
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,51 +11,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import org.example.currency_tracker.ui.coin_details.CoinDetailsScreen
-import org.example.currency_tracker.ui.coin_details.view_model.CoinDetailsViewModel
 import org.example.currency_tracker.ui.coin_list.CoinListScreen
-import org.example.currency_tracker.ui.coin_list.view_model.CoinListEvents
-import org.example.currency_tracker.ui.coin_list.view_model.CoinListViewModel
-import org.example.currency_tracker.ui.util.EventHandler
-import org.example.currency_tracker.ui.util.NavControllerManager
+import org.example.currency_tracker.ui.composable.ContentError
+import org.example.currency_tracker.ui.composable.ContentLoading
+import org.example.currency_tracker.ui.composable.ContentVisible
+import org.example.currency_tracker.ui.shared.ui_state.ContentStatus
+import org.example.currency_tracker.ui.shared.view_model.MainCoinViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun AndroidNavigation() {
-    val navController = NavControllerManager.init()
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Box(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
                 .padding(innerPadding)
         ) {
-            NavHost(
-                navController = navController,
-                startDestination = CoinList
-            ) {
-                composable<CoinList> {
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    EventHandler(viewModel.event) { event, _ ->
-                        when (event) {
-                            is CoinListEvents.NavigateToCoinDetails -> navController.navigate(
-                                CoinDetails(event.id)
-                            )
-                        }
+            val viewModel = koinViewModel<MainCoinViewModel>()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            ContentLoading(isVisible = state.contentStatus == ContentStatus.LOADING)
+            ContentVisible(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
+                Crossfade(
+                    modifier = Modifier.fillMaxSize(),
+                    targetState = state.isCoinListVisible
+                ) { isCoinList ->
+                    if (isCoinList) {
+                        CoinListScreen(state = state, interactions = viewModel)
+                    } else {
+                        CoinDetailsScreen(state = state, interactions = viewModel)
                     }
-                    CoinListScreen(state = state, interactions = viewModel)
                 }
-                composable<CoinDetails> {
-                    val viewModel = koinViewModel<CoinDetailsViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    EventHandler(viewModel.event) { event, _ ->
 
-                    }
-                    CoinDetailsScreen(state = state, interactions = viewModel)
-                }
             }
+            ContentError(
+                isVisible = state.contentStatus == ContentStatus.FAILURE,
+                onTryAgain = viewModel::initData
+            )
         }
     }
 }
